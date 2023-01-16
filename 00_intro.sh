00238d6a10f67180af7154caa1a452fced54cc0e
linuxcram <120643104+linuxcram@users.noreply.github.com>
2023-01-16 08:20:55 +0100

    Add files via upload

---
+++ 00_intro.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# exit on command fail
+set -o errexit
+string="\n >\n ->\n -->\n\n\tWelcome to Linux.\n\n\tLet's learn Bash scripting.\n\n -->\n ->\n >"
+clear
+echo -en "\e[5m\e[34m\e[42m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo
+for i in $string
+  do
+  #echo -n "$i "
+  echo -e "\e[1m\e[32m\e[44m$i \e[0m\c"
+  sleep 0.3
+done
+echo
+echo
+echo -e "\e[5m\e[34m\e[42m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo
+exit 0
